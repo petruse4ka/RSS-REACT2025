98ca926342c2d209581d06f7bc02b231ab89e6c7
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-10 23:32:37 +0200

    chore: upgrade tailwind to v4

--- first-project/vite.config.ts
+++ first-project/vite.config.ts
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
 });
