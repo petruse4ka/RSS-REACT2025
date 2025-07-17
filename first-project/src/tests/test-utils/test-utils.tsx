3b57ef7e3a8ac8f7827775eab473df75bcf93549
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-17 03:21:21 +0200

    chore: install and configure React Testing Library

---
+++ first-project/src/tests/test-utils/test-utils.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react';
+import type { RenderOptions } from '@testing-library/react';
+import type { ReactElement } from 'react';
+
+const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
+  render(ui, { ...options });
+
+export * from '@testing-library/react';
+export { customRender as render };
