c47fcd763fb4688076b754eec1050991745af3d7
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-18 20:47:36 +0200

    test: add test for the correct search query prop for the main component

--- first-project/src/components/main/main.test.tsx
+++ first-project/src/components/main/main.test.tsx
@@ -1,6 +1,17 @@
 import { render, screen, waitForElementToBeRemoved } from '@/tests/test-utils/test-utils';
 import Main from '../main/main';
 import { ERROR_TEXTS } from '@/constants';
+import App from '@/App';
+
+const localStorageMock = {
+  getItem: vi.fn(),
+  setItem: vi.fn(),
+  clear: vi.fn(),
+};
+
+Object.defineProperty(window, 'localStorage', {
+  value: localStorageMock,
+});
 
 test('Main component renders loader when loading state is true and removes it when loading is false', async () => {
   render(<Main searchQuery="test" />);
@@ -30,6 +41,27 @@ test('Main component does not render cards list and error button when loading st
 test('Main component does not render cards list and error button when error state is true and renders error message', async () => {
   render(<Main searchQuery="simulated-error-404" />);
 
+  expect(screen.queryByTestId('cards-list')).not.toBeInTheDocument();
+  expect(screen.queryByTestId('error-button')).not.toBeInTheDocument();
+
+  await waitForElementToBeRemoved(() => screen.queryByTestId('main-loader'));
+
+  expect(screen.queryByTestId('cards-list')).not.toBeInTheDocument();
+  expect(screen.getByTestId('list-error-message')).toBeInTheDocument();
+  expect(screen.getByTestId('list-error-message')).toHaveTextContent(ERROR_TEXTS.FETCH_ERROR);
+  expect(screen.queryByTestId('error-button')).toBeInTheDocument();
+});
+
+test('Main component receives searchQuery prop from localStorage and starts loading data', async () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  localStorageMock.getItem.mockReturnValue('simulated-error-404');
+
+  render(<App />);
+
+  expect(screen.getByTestId('main-loader')).toBeInTheDocument();
   expect(screen.queryByTestId('cards-list')).not.toBeInTheDocument();
   expect(screen.queryByTestId('error-button')).not.toBeInTheDocument();
 
