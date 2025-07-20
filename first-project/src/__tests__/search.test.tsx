49f7bb51d8d5b3d06fb6de2e3683c5c9be659684
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-20 18:33:53 +0200

    refactor: change file names to kebab-case

--- first-project/src/__tests__/search.test.tsx
+++ first-project/src/__tests__/search.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@/__tests__/test-utils/test-utils';
-import Search from '../components/search/search-new';
+import Search from '../components/search/search';
 import { SEARCH_TEXTS } from '@/constants';
 
 const localStorageMock = {
