cb99bd56c5ba0a02b0d876ffe31ca919f4a24449
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-22 22:59:16 +0200

    refactor: change the loader component into a functional

--- first-project/src/components/ui/loader.tsx
+++ first-project/src/components/ui/loader.tsx
@@ -1,5 +1,3 @@
-import { PureComponent } from 'react';
-
 type Props = {
   classNameSpinner: string;
   classNameText: string;
@@ -7,22 +5,19 @@ type Props = {
   dataTestId: string;
 };
 
-export default class Loader extends PureComponent<Props> {
-  render() {
-    const { classNameSpinner, classNameText, text, dataTestId } = this.props;
-    const defaultSpinnerClasses = `w-12 h-12 animate-spin rounded-full border-2 border-t-transparent`;
-    const defaultTextClasses = `font-medium`;
+export default function Loader({ classNameSpinner, classNameText, text, dataTestId }: Props) {
+  const defaultSpinnerClasses = `w-12 h-12 animate-spin rounded-full border-2 border-t-transparent`;
+  const defaultTextClasses = `font-medium`;
 
-    return (
-      <div data-testid={dataTestId} className={`flex flex-col items-center justify-center gap-3`}>
-        <div
-          data-testid="loader-spinner"
-          className={`${defaultSpinnerClasses} ${classNameSpinner}`}
-        ></div>
-        <div data-testid="loader-text" className={`${defaultTextClasses} ${classNameText}`}>
-          {text}
-        </div>
+  return (
+    <div data-testid={dataTestId} className={`flex flex-col items-center justify-center gap-3`}>
+      <div
+        data-testid="loader-spinner"
+        className={`${defaultSpinnerClasses} ${classNameSpinner}`}
+      ></div>
+      <div data-testid="loader-text" className={`${defaultTextClasses} ${classNameText}`}>
+        {text}
       </div>
-    );
-  }
+    </div>
+  );
 }
