62c426e325ae36656d7bfe992b86b4ed17c6b566
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-22 23:55:55 +0200

    refactor: change the card component into a functional

--- first-project/src/components/cards-list/card.tsx
+++ first-project/src/components/cards-list/card.tsx
@@ -1,31 +1,28 @@
-import { PureComponent } from 'react';
 import type { CardData } from '@/types/interfaces';
 
 type Props = {
   card: CardData;
 };
 
-export default class CardItem extends PureComponent<Props> {
-  render() {
-    const { imageUrl, title, description } = this.props.card;
+export default function CardItem({ card }: Props) {
+  const { imageUrl, title, description } = card;
 
-    return (
-      <li
-        data-testid="card-item"
-        className="overflow-hidden rounded-lg bg-indigo-900 transition-all duration-300 hover:scale-102"
-      >
-        <div className="h-50 overflow-hidden">
-          <img
-            src={imageUrl}
-            alt={title}
-            className="h-full w-full bg-cyan-300 object-cover object-center"
-          />
-        </div>
-        <div className="p-4">
-          <h2 className="mb-2 text-xl font-semibold text-cyan-300">{title}</h2>
-          <p className="text-sm text-gray-300">{description}</p>
-        </div>
-      </li>
-    );
-  }
+  return (
+    <li
+      data-testid="card-item"
+      className="overflow-hidden rounded-lg bg-indigo-900 transition-all duration-300 hover:scale-102"
+    >
+      <div className="h-50 overflow-hidden">
+        <img
+          src={imageUrl}
+          alt={title}
+          className="h-full w-full bg-cyan-300 object-cover object-center"
+        />
+      </div>
+      <div className="p-4">
+        <h2 className="mb-2 text-xl font-semibold text-cyan-300">{title}</h2>
+        <p className="text-sm text-gray-300">{description}</p>
+      </div>
+    </li>
+  );
 }
