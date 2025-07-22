46bd0ba97734e55184591883ce05f4c4a7476462
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-22 23:57:34 +0200

    refactor: change the cards list component into a functional

--- first-project/src/components/cards-list/cards-list.tsx
+++ first-project/src/components/cards-list/cards-list.tsx
@@ -1,4 +1,3 @@
-import { PureComponent } from 'react';
 import type { CardData } from '@/types/interfaces';
 import { ERROR_TEXTS } from '@/constants';
 import CardItem from './card';
@@ -7,22 +6,18 @@ type Props = {
   cards: CardData[];
 };
 
-export default class CardsList extends PureComponent<Props> {
-  render() {
-    const { cards } = this.props;
-
-    return cards.length > 0 ? (
-      <ul data-testid="cards-list" className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
-        {cards.map((card) => (
-          <CardItem key={card.id} card={card} />
-        ))}
-      </ul>
-    ) : (
-      <div className="flex min-h-[300px] flex-col items-center justify-center">
-        <div data-testid="list-error-message" className="mb-4 text-xl font-semibold text-red-500">
-          {ERROR_TEXTS.FETCH_ERROR}
-        </div>
+export default function CardsList({ cards }: Props) {
+  return cards.length > 0 ? (
+    <ul data-testid="cards-list" className="grid grid-cols-1 gap-5 lg:grid-cols-2 xl:grid-cols-3">
+      {cards.map((card) => (
+        <CardItem key={card.id} card={card} />
+      ))}
+    </ul>
+  ) : (
+    <div className="flex min-h-[300px] flex-col items-center justify-center">
+      <div data-testid="list-error-message" className="mb-4 text-xl font-semibold text-red-500">
+        {ERROR_TEXTS.FETCH_ERROR}
       </div>
-    );
-  }
+    </div>
+  );
 }
