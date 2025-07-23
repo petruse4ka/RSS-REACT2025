fc4aae0c9f506eb5018437a9e1defdc67d68c844
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-23 03:15:56 +0200

    refactor: pass initializer function of the ls hook into useState

--- first-project/src/hooks/use-local-storage.tsx
+++ first-project/src/hooks/use-local-storage.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 
 export default function useLocalStorage(key: string, defaultValue: string) {
-  const [storedValue, setStoredValue] = useState<string>(() => {
+  const createInitialStoredValue = () => {
     const item = window.localStorage.getItem(key);
     return item ? item : defaultValue;
-  });
+  };
+
+  const [storedValue, setStoredValue] = useState<string>(createInitialStoredValue);
 
   const setValue = (value: string) => {
     setStoredValue(value);
