1640cc893878cc5eee28d463b8fa530dc99fdee5
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-07-29 23:10:35 +0200

    feat: add the mock data in dev environment

--- first-project/src/main.tsx
+++ first-project/src/main.tsx
@@ -4,6 +4,10 @@ import { RouterProvider } from 'react-router-dom';
 import '@/styles/index.scss';
 import { router } from './router/routes.tsx';
 
+if (import.meta.env.DEV) {
+  import('./__tests__/mocks/browser.ts');
+}
+
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
