ab03de9f193917c982a442f76cda9e688408053e
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-08-12 04:02:40 +0200

    feat: add the mock data for browser in dev environment

--- first-project/src/main.tsx
+++ first-project/src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client';
 import '@/styles/index.scss';
 import AppWrapper from './components/providers/app-wrapper';
 
+if (import.meta.env.DEV) {
+  import('./__tests__/mocks/browser.ts');
+}
+
 const root = document.getElementById('root');
 if (root) {
   createRoot(root).render(
