3f1d26edc747bb9e44f03cdafeb9685d974bf44b
Konstantin Petrov <43613041+petruse4ka@users.noreply.github.com>
2025-08-30 04:11:24 +0200

    fix: change padding in table cells for smalle screen widths

--- third-project/src/components/emission-table/row.tsx
+++ third-project/src/components/emission-table/row.tsx
@@ -22,10 +22,10 @@ export default function CountryRow({ country, mainTableFields, additionalTableFi
     <tr
       className={`hover:bg-scooter-400 dark:hover:bg-shamrock-400 group transition-colors duration-300`}
     >
-      <td className="group-hover:bg-scooter-400 dark:group-hover:bg-shamrock-400 sticky left-0 bg-zinc-100 px-6 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 dark:bg-zinc-900 dark:text-zinc-50">
+      <td className="group-hover:bg-scooter-400 dark:group-hover:bg-shamrock-400 sticky left-0 bg-zinc-100 px-2 py-4 text-sm text-zinc-700 transition-colors duration-300 sm:px-6 lg:whitespace-nowrap dark:bg-zinc-900 dark:text-zinc-50">
         {country.name}
       </td>
-      <td className="px-6 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 dark:text-zinc-50">
+      <td className="px-2 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 sm:px-6 dark:text-zinc-50">
         {country.iso_code || translations.table.noData}
       </td>
       {mainTableFields.map((field) => {
@@ -33,7 +33,7 @@ export default function CountryRow({ country, mainTableFields, additionalTableFi
         return (
           <td
             key={field.key}
-            className="px-6 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 dark:text-zinc-50"
+            className="px-2 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 sm:px-6 dark:text-zinc-50"
           >
             {formatValue(fieldValue)}
           </td>
@@ -44,7 +44,7 @@ export default function CountryRow({ country, mainTableFields, additionalTableFi
         return (
           <td
             key={field.key}
-            className="px-6 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 dark:text-zinc-50"
+            className="px-2 py-4 text-sm whitespace-nowrap text-zinc-700 transition-colors duration-300 sm:px-6 dark:text-zinc-50"
           >
             {formatValue(fieldValue)}
           </td>
